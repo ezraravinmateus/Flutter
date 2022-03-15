84319ca8a78d8094f9c43f572f44c9366b4461b9
Ezra Ravin <ezraravin@gmail.com>
2022-03-15 21:26:46 +0700

    Update

--- Ninja Course Flutter/Custom Class/lib/main.dart
+++ Ninja Course Flutter/Custom Class/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() => runApp(MaterialApp
 (
@@ -14,11 +15,11 @@ class QuoteList extends StatefulWidget
 
 class _QuoteListState extends State<QuoteList> 
 {
-	List<String> quotes = 
+	List<Quote> quotes = 
 	[
-		"Be yourself; everyone else is already taken",
-		"I have nothing to declare except my genius",
-		"The truth is rarely pure and never simple"
+		Quote(author: "Oscar Wilde", text: "Be yourself; everyone else is already taken"),
+		Quote(author: "Oscar Wilde", text: "Be yourself; everyone else is already taken"),
+		Quote(author: "Oscar Wilde", text: "Be yourself; everyone else is already taken")
 	];
 
 	
@@ -38,7 +39,7 @@ class _QuoteListState extends State<QuoteList>
 			),
 			body: Column
 			(
-				children: quotes.map((quote) => Text(quote)).toList(),
+				children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
 			),
 		);
 	}
