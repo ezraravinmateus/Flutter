9a423668fe756d6e6466eee3ea6f187ed83a080b
Ezra Ravin <ezraravin@gmail.com>
2022-03-15 17:03:11 +0700

    Update

--- Ninja Course Flutter/Ninja ID Card/lib/main.dart
+++ Ninja Course Flutter/Ninja ID Card/lib/main.dart
@@ -28,6 +28,18 @@ class NinjaCard extends StatelessWidget
 					crossAxisAlignment: CrossAxisAlignment.start,
 					children: <Widget>
 					[
+						Center(
+							child: CircleAvatar
+							(
+								backgroundImage: AssetImage('assets/thumb.jpg'),
+								radius: 40,
+							),
+						),
+						Divider
+						(
+							height: 90,
+							color: Colors.grey[800],
+						),
 						Text
 						(
 							"NAME",
@@ -73,6 +85,28 @@ class NinjaCard extends StatelessWidget
 							),
 						),
 						SizedBox(height: 30,),
+						Row
+						(
+							children: <Widget>
+							[
+								Icon
+								(
+									Icons.email, 
+									color: Colors.grey[400],
+								),
+								SizedBox(width: 10),
+								Text
+								(
+									"chun.li@thenetninja.co.uk",
+									style: TextStyle
+									(
+										color: Colors.grey[400],
+										fontSize: 18,
+										letterSpacing: 1.0
+									),
+								)
+							],
+						)
 					],
 				),
 			),
