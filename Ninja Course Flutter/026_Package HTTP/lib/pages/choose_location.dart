b1a21a7052705ee2d79cd1e801cf9d3345b9a17f
Ezra Ravin Mateus <ezraravin@gmail.com>
2022-03-16 21:30:37 +0700

    Add

--- Ninja Course Flutter/026_Package HTTP/lib/pages/choose_location.dart
+++ Ninja Course Flutter/026_Package HTTP/lib/pages/choose_location.dart
@@ -9,31 +9,7 @@ class ChooseLocation extends StatefulWidget
 class _ChooseLocationState extends State<ChooseLocation> 
 {
 
-	void getData() async
-	{
-		// Simulate network request for username
-		String username = await Future.delayed(Duration(seconds: 3), ()
-		{
-			return "Yoshi";
-		});
-
-		// Simulate network request to get Bio of username
-		String bio = await Future.delayed(Duration(seconds: 2), ()
-		{
-			return "Vega, musician & egg collector";
-		});
- 
-		print("$username - $bio");
-	}
-
-	int counter = 0;
-	@override
-	void initState() 
-	{
-		super.initState();
-		getData();
-		print("Hey there");
-	}
+	
 
 	@override
 	Widget build(BuildContext context) 
--- Ninja Course Flutter/026_Package HTTP/lib/pages/loading.dart
+++ Ninja Course Flutter/026_Package HTTP/lib/pages/loading.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart';
 class Loading extends StatefulWidget 
 {
   const Loading({ Key? key }) : super(key: key);
@@ -8,7 +8,23 @@ class Loading extends StatefulWidget
   State<Loading> createState() => _LoadingState();
 }
 
-class _LoadingState extends State<Loading> {
+class _LoadingState extends State<Loading> 
+{
+	void getData() async
+	{
+		Response response = await get('https://jsonplaceholder.typicode.com/todos/1');
+		print(response.body);
+	}
+
+	int counter = 0;
+	@override
+	void initState() 
+	{
+		super.initState();
+		getData();
+		print("Hey there");
+	}
+
 	@override
 	Widget build(BuildContext context) 
 	{
