e3bb831166cc6f3b9e27078a82cee67156885c4f
Ezra Ravin Mateus <ezraravin@gmail.com>
2022-03-16 15:45:39 +0700

    Add

--- Ninja Course Flutter/020_Extract Widgets/lib/main.dart
+++ Ninja Course Flutter/020_Extract Widgets/lib/main.dart
@@ -24,39 +24,7 @@ class _QuoteListState extends State<QuoteList>
 
 	Widget quoteTemplate(quote)
 	{
-		return Card
-		(
-			margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
-			child: Padding(
-			  padding: const EdgeInsets.all(12.0),
-			  child: Column
-			  (
-				  crossAxisAlignment: CrossAxisAlignment.stretch,
-			  	children: <Widget>
-			  	[
-			  		Text
-			  		(
-			  			quote.text,
-			  			style: TextStyle
-			  			(
-			  				fontSize: 16,
-			  				color: Colors.grey[600],
-			  			),
-			  		),
-			  		SizedBox(height: 6,),
-			  		Text
-			  		(
-			  			quote.author,
-			  			style: TextStyle
-			  			(
-			  				fontSize: 12,
-			  				color: Colors.grey[600],
-			  			),
-			  		)
-			  	],
-			  ),
-			)
-		);
+		return new QuoteCard(quote : quote);
 	}
 
 	@override
@@ -77,6 +45,50 @@ class _QuoteListState extends State<QuoteList>
 				crossAxisAlignment: CrossAxisAlignment.start,
 				children: quotes.map((quote) => quoteTemplate(quote)).toList(),
 			),
+		);
+	}
+}
+
+class QuoteCard extends StatelessWidget 
+{
+
+	final Quote quote;
+	QuoteCard({required this.quote});
+
+	@override
+	Widget build(BuildContext context) {
+		return Card
+		(
+			margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+			child: Padding(
+			padding: const EdgeInsets.all(12.0),
+			child: Column
+			(
+				crossAxisAlignment: CrossAxisAlignment.stretch,
+				children: <Widget>
+				[
+					Text
+					(
+						quote.text,
+						style: TextStyle
+						(
+							fontSize: 16,
+							color: Colors.grey[600],
+						),
+					),
+					SizedBox(height: 6,),
+					Text
+					(
+						quote.author,
+						style: TextStyle
+						(
+							fontSize: 12,
+							color: Colors.grey[600],
+						),
+					)
+				],
+			),
+			)
 		);
 	}
 }
