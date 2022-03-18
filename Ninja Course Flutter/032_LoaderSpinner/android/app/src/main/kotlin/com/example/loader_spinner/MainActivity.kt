d8205687e8234aabf290a673f5d237631799e32d
Ezra Ravin Mateus <ezraravin@gmail.com>
2022-03-18 13:53:01 +0700

    Add

---
+++ Ninja Course Flutter/032_LoaderSpinner/android/app/src/main/kotlin/com/example/loader_spinner/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.loader_spinner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- Ninja Course Flutter/032_Loaders & Spinners/android/app/src/main/kotlin/com/example/loaders_and_spinners/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.loaders_and_spinners
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
