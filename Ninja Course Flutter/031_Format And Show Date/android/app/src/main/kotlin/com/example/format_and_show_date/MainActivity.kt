53bb676d99cf44263b6352c3daa4b567177e62fe
Ezra Ravin Mateus <ezraravin@gmail.com>
2022-03-18 13:18:20 +0700

    Add

---
+++ Ninja Course Flutter/031_Format And Show Date/android/app/src/main/kotlin/com/example/format_and_show_date/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.format_and_show_date
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Ninja Course Flutter/031_Format And Show Date/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:format_and_show_date/pages/home.dart';
+import 'package:format_and_show_date/pages/choose_location.dart';
+import 'package:format_and_show_date/pages/loading.dart';
+
+void main() => runApp(MaterialApp
+(
+	initialRoute: '/',
+
+	routes:
+	{
+		'/' : (context) => Loading(),
+		'/home' : (context) => Home(),
+		'/location' : (context) => ChooseLocation(),
+	},
+));
