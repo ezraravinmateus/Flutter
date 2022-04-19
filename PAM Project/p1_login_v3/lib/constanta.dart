a1d6f8caa880176494a2f3123b953efd3422215b
Ezra Ravin Mateus <ezraravin.m@gmail.com>
2022-04-19 16:26:52 +0700

    Edit

--- PAM Project/p1_login_v3/lib/constanta.dart
+++ PAM Project/p1_login_v3/lib/constanta.dart
@@ -5,6 +5,7 @@ Color colorPrimary = Color.fromRGBO(117, 23, 176, 1);
 Color colorSecondary = Color.fromARGB(255, 176, 92, 23);
 
 //Api Url
-String baseUrl = "https://ringgas.id/develop/mobapps/flutter/";
+// String baseUrl = "https://ringgas.id/develop/mobapps/flutter/";
+String baseUrl = 'https://www.ringgas.id/develop/01_it604_pam/1320023/mobapps/';
 
 String loginUrl = "get_login.php";
